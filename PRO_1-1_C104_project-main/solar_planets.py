a9fd4c6d7e28dc078a36853212a5acab27e75f7f
ThiagoAlves13 <93220975+ThiagoAlves13@users.noreply.github.com>
2023-04-11 18:05:07 -0300

    Add files via upload

---
+++ PRO_1-1_C104_project-main/solar_planets.py
@@ -0,0 +1,54 @@
+import cv2
+
+img = cv2.imread("solar-system.jpg")
+
+cv2.putText(img,
+            "Sol",
+             (100,80),
+             cv2.FONT_HERSHEY_COMPLEX,
+             2,
+             (0,0,255)
+             )
+
+cv2.putText(img,
+            "Mercurio",
+             (110,185),
+             cv2.FONT_HERSHEY_COMPLEX,
+             0.5,
+             (255,255,255)
+             )
+cv2.putText(img,
+            "Venus",
+             (190,175),
+             cv2.FONT_HERSHEY_COMPLEX,
+             0.5,
+             (255,255,255)
+             )
+cv2.putText(img,
+            "Terra",
+             (285,170),
+             cv2.FONT_HERSHEY_COMPLEX,
+             0.5,
+             (255,255,255)
+             )
+cv2.putText(img,
+            "Marte",
+             (375,180),
+             cv2.FONT_HERSHEY_COMPLEX,
+             0.5,
+             (255,255,255)
+             )
+cv2.putText(img,
+            "Jupiter",
+             (555,50),
+             cv2.FONT_HERSHEY_COMPLEX,
+             0.5,
+             (255,255,255)
+             )
+
+
+
+cv2.imshow("Resultado",img)
+cv2.waitKey(0)
+
+cv2.imwrite("Solar_systemwithname.jpg",img)
